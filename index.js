c322e215b8de23137864ace7eac315b2a8ccd198
Tom Nielsen <tanielsen@gmail.com>
2022-10-12 17:27:42 +0100

    session class

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 
-const options = { runScripts: "dangerously", resources: "usable" }
-JSDOM.fromURL("https://saltcorn.com/", options).then(dom => {
-    console.log(dom.serialize());
-});
+
+class E2ESession {
+    constructor() {
+        const cookieJar = new jsdom.CookieJar();
+        this.options = { cookieJar, runScripts: "dangerously", resources: "usable" }
+        this.dom = null
+    }
+
+    async goto(url) {
+        this.dom = await JSDOM.fromURL(url, this.options)
+
+    }
+    contains(str) {
+        this.assert(this.dom.serialize().includes(str), "contains " + str)
+        return this
+    }
+    assert(b, s) {
+        if (!b) throw new Error(s)
+    }
+
+}
+
+const go = async () => {
+    const s = new E2ESession()
+    await s.goto("https://saltcorn.com/")
+    s.contains("Focus on database-backed")
+    s.contains("Focus on datsabase-backed")
+}
+
+go()
